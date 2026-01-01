759048a9fe64adc9b8b6b7212de8a928f6a3686c
Scott Jespersen <scott.d.jespersen@gmail.com>
2025-12-31 18:36:02 -0800

    I think it's working!

--- OrganEncoder/OrganEncoder.ino
+++ OrganEncoder/OrganEncoder.ino
@@ -1,17 +1,20 @@
-/* Send MIDI over serial. */
+/*
+ * Encoder for the Epworth organ.
+ *
+ * Currently this only scans the stop tabs (i.e. keys) and displays the stop state using the
+ * panel-mounted LEDs. Soon enough we'll add logic for the entire combination action (pistons,
+ * toe spades, etc.).
+ */
 
-#include <MIDI.h>
 #include <Wire.h>
 #include <MCP23017.h>
 
 
 // To save on CPU cycles, don't poll the stop tab keys too often. We could keep adjusting this
 // number upward as long as we don't drop any key presses.
-constexpr int STOP_TAB_KEY_POLL_ITVL_MICROS = 2000;
+constexpr uint16_t STOP_TAB_KEY_POLL_ITVL_MICROS = 1500;
 
 constexpr uint8_t N_DEBOUNCE_STEPS_STOP_TABS = 5;
-
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
 
 MCP23017 stopTabMcps[4][2] = {
@@ -21,12 +24,11 @@ MCP23017 stopTabMcps[4][2] = {
   {MCP23017(0x26), MCP23017(0x27)},
 };
 
-// Not using a debouncing library because they all seem to need direct pin access.
-// We are debouncing readings from GPIO expanders.
-// Keep the last 5 readings from each button. All the buttons are active low, so the
-// "natural" initial state is all 1s. Note however that each bank has *15* buttons,
-// not 16, so we hereby decree that the LSB of will always be 1 by convention. Any
-// other code that detects key presses must take care to assume this.
+// Not using a debouncing library because they all seem to need direct pin access. We are
+// debouncing readings from GPIO expanders. We will keep the last 5 readings from each button. All
+// the buttons are active low, so the "natural" initial state is all 1s. Note however that each
+// bank has *15* buttons, not 16, so we hereby decree that the LSB will always be 1 by convention.
+// Any other code that detects key presses must take care to assume this.
 uint16_t stopTabKeyReadings[4][N_DEBOUNCE_STEPS_STOP_TABS] = {
   {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF},
   {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF},
@@ -39,26 +41,34 @@ uint16_t stopState[4] = {0, 0, 0, 0};
 
 elapsedMicros sinceLastStopTabKeyScan = 0;
 
+uint16_t reverseByte(uint16_t b) {
+   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+   return b;
+}
+
 void stopTabKeyPressed(uint8_t division, uint8_t button) {
-  // Button j, division i was pressed...
-  // TODO: Take meaningful action in this condition (emit MIDI, toggle stop state)
-  Serial.print("We have a falling edge for division ");
-  Serial.print(division);
-  Serial.print(", button #");
-  Serial.println(button);
+  uint16_t stopMask = 1 << (15 - button);
+  stopState[division] ^= stopMask;
+
+  // The schematic makes this clear, but it may be counterintuitive: Because of the pin layout on
+  // the MCP23017, the LEDs are effectively in reverse order from the switches. That is, although
+  // GPA1 -> D1 ... GPA7 -> D7, we actually have GPB0 -> Sw7In ... GPB6 -> Sw1In. Ergo, because the
+  // stop state bits correspond L to R to the LEDs, we need to reverse them before writing out.
+  // Finally, stop on == LED on == logic level LOW because we are sinking current through the LEDs,
+  // so need to invert all bits.
+  uint8_t leftHalf = reverseByte(~stopState[division] >> 8);
+  uint8_t rightHalf = reverseByte(~stopState[division] & 0xFF);
+
+  stopTabMcps[division][0].writePort(MCP23017Port::A, (leftHalf & 0x7F) << 1); // LSB is input
+  stopTabMcps[division][0].writePort(MCP23017Port::B, leftHalf & 0x80); // MSB only; rest are inputs
+  stopTabMcps[division][1].writePort(MCP23017Port::A, rightHalf & 0x7F); // MSB is not connected
 
   uint8_t controlNumber = 102 + button;
-  uint8_t controlValue = 127; // or 0; depends on stopState!
+  uint8_t controlValue = stopState[division] & stopMask ? 127 : 0;
 
-  Serial.print("Sending MIDI CC {number: ");
-  Serial.print(controlNumber);
-  Serial.print(", value: ");
-  Serial.print(controlValue);
-  Serial.print(", channel: ");
-  Serial.print(division);
-  Serial.println("}");
-
-  // MIDI.sendControlChange(controlNumber, controlValue, division);
+  usbMIDI.sendControlChange(controlNumber, controlValue, division);
 }
 
 void pollStopTabKeys() {
@@ -66,16 +76,15 @@ void pollStopTabKeys() {
     // Overwrite oldest history entry with current.
     stopTabKeyReadings[i][stopTabPollCtr] = 0;
 
-    // TODO: Determine whether pin GPA7/GPB7 is the MSB or LSB! It matters greatly! The
-    // current implementation assumes it is the LSB.
+    // In the following lines, for both ports A and B, the MSB corresponds to pin 7 and LSB to pin 0.
 
     // For the first chip of every pair, need to read both ports, as button 8 is routed
-    // to GPA0, though buttons 1-7 are on GPB0-6.
+    // to GPA0, though buttons 1-7 are on GPB0-6 (in reverse order).
     stopTabKeyReadings[i][stopTabPollCtr] |= stopTabMcps[i][0].readPort(MCP23017Port::B) & 0x7F;
     stopTabKeyReadings[i][stopTabPollCtr] <<= 1;
     stopTabKeyReadings[i][stopTabPollCtr] |= stopTabMcps[i][0].readPort(MCP23017Port::A) & 0x01;
     stopTabKeyReadings[i][stopTabPollCtr] <<= 7;
-    // For the second chip of every pair, only need to read port B, ignoring last pin, since
+    // For the second chip of every pair, only need to read port B, ignoring GPB7 (MSB), since
     // there are only 7 buttons hooked up to this one (GPB0-6).
     stopTabKeyReadings[i][stopTabPollCtr] |= stopTabMcps[i][1].readPort(MCP23017Port::B) & 0x7F;
     stopTabKeyReadings[i][stopTabPollCtr] <<= 1;
@@ -124,20 +133,21 @@ void setup() {
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 2; j++) {
       stopTabMcps[i][j].init();
-      stopTabMcps[i][j].portMode(MCP23017Port::A, 0b10000000);
-      stopTabMcps[i][j].portMode(MCP23017Port::B, 0b11111110);
-      stopTabMcps[i][j].writeRegister(MCP23017Register::GPIO_A, 0x7F);
-      stopTabMcps[i][j].writeRegister(MCP23017Register::GPIO_B, 0x01);
+      stopTabMcps[i][j].portMode(MCP23017Port::A, 0x01);
+      stopTabMcps[i][j].portMode(MCP23017Port::B, 0x7F);
+      stopTabMcps[i][j].writeRegister(MCP23017Register::GPIO_A, 0xFE);
+      stopTabMcps[i][j].writeRegister(MCP23017Register::GPIO_B, 0x80);
     }
   }
-
-  Serial.begin(9600);
-  MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
 void loop() {
   if (sinceLastStopTabKeyScan > STOP_TAB_KEY_POLL_ITVL_MICROS) {
     pollStopTabKeys();
     sinceLastStopTabKeyScan = 0;
+  }
+
+  // MIDI Controllers should discard incoming MIDI messages.
+  while (usbMIDI.read()) {
   }
 }
