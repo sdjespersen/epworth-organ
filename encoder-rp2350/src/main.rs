f226d0a20852ca606c62f5c826a426d74c8aed52
Scott Jespersen <sjespersen@myst.ai>
2026-01-28 18:10:23 -0800

    Batch LED changes

--- encoder-rp2350/src/main.rs
+++ encoder-rp2350/src/main.rs
@@ -10,6 +10,7 @@ use core::sync::atomic::{AtomicU16, Ordering};
 use debouncer::Debouncer;
 use embassy_embedded_hal::shared_bus::blocking::i2c::I2cDevice;
 use embassy_executor::Spawner;
+use embassy_futures::select::select4;
 use embassy_rp::gpio::Pull;
 use embassy_rp::i2c;
 use embassy_rp::peripherals::I2C0;
@@ -21,6 +22,7 @@ use embassy_rp::{
 use embassy_sync::blocking_mutex::Mutex;
 use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex};
 use embassy_sync::channel::Channel;
+use embassy_sync::signal::Signal;
 use embassy_time::Timer;
 use panic_halt as _;
 use static_cell::StaticCell;
@@ -49,6 +51,8 @@ macro_rules! atomicu16_array {
 }
 
 static STOP_STATE: [AtomicU16; 4] = atomicu16_array!(0);
+static STOP_STATE_CHANGED: [Signal<CriticalSectionRawMutex, bool>; 4] =
+    [Signal::new(), Signal::new(), Signal::new(), Signal::new()];
 // TODO: This should be persistent somehow
 static PRESETS: [[AtomicU16; 4]; 8] = [
     atomicu16_array!(0),
@@ -70,52 +74,21 @@ fn reverse_byte(a: &u8) -> u8 {
     b
 }
 
-async fn read_stop_tabs(mcp_pair: &mut [I2c0Mcp; 2]) -> u16 {
-    let mut raw_button_readings = 0x01u16; // LSB always 1, by convention
-    raw_button_readings |= ((mcp_pair[0].read_gpio(Port::GPIOA).unwrap() & 0x01) as u16) << 8;
-    raw_button_readings |= ((mcp_pair[0].read_gpio(Port::GPIOB).unwrap() & 0x7F) as u16) << 9;
-    raw_button_readings |= ((mcp_pair[1].read_gpio(Port::GPIOB).unwrap() & 0x7F) as u16) << 1;
-    raw_button_readings
-}
-
-fn write_stop_state_to_leds(mcp_pair: &mut [I2c0Mcp; 2], div_stop_state: u16) {
-    let left_half = reverse_byte(&(!(div_stop_state >> 8) as u8));
-    let right_half = reverse_byte(&(!(div_stop_state & 0xFF) as u8));
-    mcp_pair[0]
-        .write_gpio(Port::GPIOA, (left_half & 0x7F) << 1)
-        .unwrap();
-    mcp_pair[0]
-        .write_gpio(Port::GPIOB, left_half & 0x80)
-        .unwrap();
-    mcp_pair[1]
-        .write_gpio(Port::GPIOA, right_half & 0x7F)
-        .unwrap();
-}
-
-// fn send_stop_change(_div: usize, _i: u8, _target_state: bool) {}
-
-fn handle_cc(mcps: &mut [[I2c0Mcp; 2]; 4], channel: u8, control_number: u8, value: u8) {
+fn handle_control_change(channel: u8, control_number: u8, value: u8) {
     let i = 117 - control_number;
     if value > 63 {
         STOP_STATE[channel as usize].fetch_or(1 << i, Ordering::SeqCst);
     } else {
         STOP_STATE[channel as usize].fetch_and(!(1 << i), Ordering::SeqCst);
     }
-    let div_stop_state = STOP_STATE[channel as usize].load(Ordering::SeqCst);
-    // TODO: Decouple stop state flushing! It's possible to get a flurry of incoming CCs (e.g. from external preset),
-    // in which case we'd like to update the internal state and only _then_ flush to MCPs. This could probably be
-    // accomplished by throttling the updates to the MCPs
-    write_stop_state_to_leds(&mut mcps[channel as usize], div_stop_state);
+    STOP_STATE_CHANGED[channel as usize].signal(true);
 }
 
-// fn send_program_change(_i: u8) {}
-
-fn handle_program_change(mcps: &mut [[I2c0Mcp; 2]; 4], value: u8) {
-    for (div, mcp_pair) in mcps.iter_mut().enumerate() {
+fn handle_program_change(value: u8) {
+    for div in 0..4 {
         let div_preset = PRESETS[value as usize][div].load(Ordering::SeqCst);
         STOP_STATE[div].store(div_preset, Ordering::SeqCst);
-        // TODO: Decouple stop state flushing. Similar motivation as above note, though this instance isn't too bad.
-        write_stop_state_to_leds(mcp_pair, div_preset);
+        STOP_STATE_CHANGED[div].signal(true);
     }
 }
 
@@ -135,20 +108,60 @@ async fn enqueue_internal(message: MidiMessage) {
 }
 
 #[embassy_executor::task]
-async fn midi_event_listener(i2c_bus: &'static I2c0Bus) {
-    let mcps = &mut get_mcps(i2c_bus);
-
+async fn midi_event_listener() {
     loop {
         let event = MIDI_EVENT_CHANNEL.receive().await;
         match event {
             MidiMessage::ProgramChange(_channel, prog) => {
-                handle_program_change(mcps, prog.into());
+                handle_program_change(prog.into());
             }
             MidiMessage::ControlChange(channel, cc_number, cc_value) => {
-                handle_cc(mcps, channel.into(), cc_number.into(), cc_value.into());
+                handle_control_change(channel.into(), cc_number.into(), cc_value.into());
             }
             _ => {} // unhandled for now
         }
+    }
+}
+
+#[embassy_executor::task]
+async fn flush_stop_state_to_leds(i2c_bus: &'static I2c0Bus) {
+    let mcps = &mut get_mcps(i2c_bus);
+
+    loop {
+        let stop_state_change = select4(
+            STOP_STATE_CHANGED[0].wait(),
+            STOP_STATE_CHANGED[1].wait(),
+            STOP_STATE_CHANGED[2].wait(),
+            STOP_STATE_CHANGED[3].wait(),
+        )
+        .await;
+        let idx: usize = {
+            if stop_state_change.is_first() {
+                0
+            } else if stop_state_change.is_second() {
+                1
+            } else if stop_state_change.is_third() {
+                2
+            } else {
+                3
+            }
+        };
+
+        let div_stop_state = STOP_STATE[idx].load(Ordering::SeqCst);
+        let left_half = reverse_byte(&(!(div_stop_state >> 8) as u8));
+        let right_half = reverse_byte(&(!(div_stop_state & 0xFF) as u8));
+        mcps[idx][0]
+            .write_gpio(Port::GPIOA, (left_half & 0x7F) << 1)
+            .unwrap();
+        mcps[idx][0]
+            .write_gpio(Port::GPIOB, left_half & 0x80)
+            .unwrap();
+        mcps[idx][1]
+            .write_gpio(Port::GPIOA, right_half & 0x7F)
+            .unwrap();
+
+        // This throttles updates a bit, so there might be mild lag when recalling presets (>= 4x this delay).
+        Timer::after_micros(200).await;
     }
 }
 
@@ -164,10 +177,15 @@ async fn scan_stop_tab_buttons(i2c_bus: &'static I2c0Bus) {
 
     loop {
         for (div, mcp_pair) in mcps.iter_mut().enumerate() {
-            stop_tab_button_debouncers[div].update(read_stop_tabs(mcp_pair).await);
+            let mut raw_reading = 0x01u16; // LSB always 1, by convention
+            raw_reading |= ((mcp_pair[0].read_gpio(Port::GPIOA).unwrap() & 0x01) as u16) << 8;
+            raw_reading |= ((mcp_pair[0].read_gpio(Port::GPIOB).unwrap() & 0x7F) as u16) << 9;
+            raw_reading |= ((mcp_pair[1].read_gpio(Port::GPIOB).unwrap() & 0x7F) as u16) << 1;
+            stop_tab_button_debouncers[div].update(raw_reading);
+
             // This iterator is almost always going to be size 0 or 1, so acquiring the mutex in the loop is fine.
             for i in stop_tab_button_debouncers[div].falling_edges() {
-                let div_stop_state = STOP_STATE[div].load(Ordering::Relaxed);
+                let div_stop_state = STOP_STATE[div].load(Ordering::SeqCst);
                 let mut midi_val = 0u8;
                 if div_stop_state >> i & 1 == 0 {
                     midi_val = 127; // currently off, toggling to on
@@ -191,6 +209,8 @@ async fn scan_pistons(
     data_pin: Peri<'static, AnyPin>,
 ) {
     let mut piston_debouncer = Debouncer::<5>::new(0xFFFF);
+    // TODO: Re-implement the save button using MIDI SysEx, so that it can live on a separate MCU and doesn't require
+    // special handling.
     let mut awaiting_save = false;
 
     // Load piston states into 74HC165 shift register
@@ -281,7 +301,8 @@ async fn main(spawner: Spawner) {
         .spawn(scan_pistons(p.PIN_2.into(), p.PIN_6.into(), p.PIN_3.into()))
         .unwrap();
     spawner.spawn(scan_stop_tab_buttons(i2c_bus)).unwrap();
-    spawner.spawn(midi_event_listener(i2c_bus)).unwrap();
+    spawner.spawn(flush_stop_state_to_leds(i2c_bus)).unwrap();
+    spawner.spawn(midi_event_listener()).unwrap();
 
     let mut led = Output::new(p.PIN_25, Level::Low);
 
