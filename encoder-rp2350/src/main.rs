7c6d8847ea9dfc8a0756c3aa0f8506ed18840e78
Scott Jespersen <sjespersen@myst.ai>
2026-01-29 21:39:20 -0800

    Preset recall now emits MIDI CCs.
    This also rather simplifies a few things:
    - Batching of LED updates is accomplished simply by waiting 1ms
    - Removes "internal MIDI bus"

--- encoder-rp2350/src/main.rs
+++ encoder-rp2350/src/main.rs
@@ -11,7 +11,6 @@ use debouncer::Debouncer;
 use defmt::warn;
 use embassy_embedded_hal::shared_bus::blocking::i2c::I2cDevice;
 use embassy_executor::Spawner;
-use embassy_futures::select::select4;
 use embassy_rp::gpio::Pull;
 use embassy_rp::peripherals::I2C0;
 use embassy_rp::peripherals::USB;
@@ -43,10 +42,7 @@ bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => InterruptHandler<USB>;
 });
 
-const GC_MESSAGE: MidiMessage = MidiMessage::ProgramChange(
-    MidiChannel::new(4), // should this be 5?
-    Program::new(0),
-);
+const GC_MESSAGE: MidiMessage = MidiMessage::ProgramChange(MidiChannel::new(4), Program::new(0));
 
 macro_rules! atomicu16_array {
     ($val:expr) => {{
@@ -60,8 +56,7 @@ macro_rules! atomicu16_array {
 }
 
 static STOP_STATE: [AtomicU16; 4] = atomicu16_array!(0);
-static STOP_STATE_CHANGED: [Signal<CriticalSectionRawMutex, bool>; 4] =
-    [Signal::new(), Signal::new(), Signal::new(), Signal::new()];
+static STOP_STATE_CHANGED: Signal<CriticalSectionRawMutex, bool> = Signal::new();
 // TODO: This should be persistent somehow
 static PRESETS: [[AtomicU16; 4]; 8] = [
     atomicu16_array!(0),
@@ -74,7 +69,6 @@ static PRESETS: [[AtomicU16; 4]; 8] = [
     atomicu16_array!(0),
 ];
 static OUTBOUND_MIDI_EVENT_BUS: Channel<CriticalSectionRawMutex, MidiMessage, 16> = Channel::new();
-static INTERNAL_MIDI_EVENT_BUS: Channel<CriticalSectionRawMutex, MidiMessage, 16> = Channel::new();
 
 static USB_CONFIG_DESCRIPTOR: ConstStaticCell<[u8; 256]> = ConstStaticCell::new([0; 256]);
 static USB_BOS_DESCRIPTOR: ConstStaticCell<[u8; 256]> = ConstStaticCell::new([0; 256]);
@@ -88,22 +82,37 @@ fn reverse_byte(a: &u8) -> u8 {
     b
 }
 
-fn handle_control_change(channel: u8, control_number: u8, value: u8) {
-    let i = 117 - control_number;
-    if value > 63 {
-        STOP_STATE[channel as usize].fetch_or(1 << i, Ordering::SeqCst);
-    } else {
-        STOP_STATE[channel as usize].fetch_and(!(1 << i), Ordering::SeqCst);
+fn stop_tab_midi_cc_message(div: usize, i: u8, div_stop_state: u16) -> MidiMessage {
+    let mut midi_val = 0u8;
+    if div_stop_state >> i & 1 == 1 {
+        midi_val = 127;
     }
-    STOP_STATE_CHANGED[channel as usize].signal(true);
+    MidiMessage::ControlChange(
+        MidiChannel::from(div as u8),
+        Control::from(117 - i),
+        Value7::from(midi_val),
+    )
 }
 
-fn handle_program_change(value: u8) {
+fn save_preset(value: u8) {
+    for div in 0..4 {
+        PRESETS[value as usize][div]
+            .store(STOP_STATE[div].load(Ordering::SeqCst), Ordering::SeqCst);
+    }
+}
+
+async fn recall_preset(value: u8) {
     for div in 0..4 {
         let div_preset = PRESETS[value as usize][div].load(Ordering::SeqCst);
         STOP_STATE[div].store(div_preset, Ordering::SeqCst);
-        STOP_STATE_CHANGED[div].signal(true);
+        // Now we need to emit 15 MIDI CCs...skipping i = 0 (LSB) which doesn't correspond to physical hardware
+        for i in 1..=15 {
+            OUTBOUND_MIDI_EVENT_BUS
+                .send(stop_tab_midi_cc_message(div, i, div_preset))
+                .await;
+        }
     }
+    STOP_STATE_CHANGED.signal(true);
 }
 
 fn get_mcps(i2c_bus: &'static I2c0Bus) -> [[I2c0Mcp; 2]; 4] {
@@ -115,67 +124,32 @@ fn get_mcps(i2c_bus: &'static I2c0Bus) -> [[I2c0Mcp; 2]; 4] {
     })
 }
 
-async fn enqueue_midi_out_and_in(message: MidiMessage) {
-    OUTBOUND_MIDI_EVENT_BUS.sender().send(message).await;
-    INTERNAL_MIDI_EVENT_BUS.sender().send(message).await;
-}
-
 #[embassy_executor::task]
-async fn midi_event_listener() {
-    loop {
-        let event = INTERNAL_MIDI_EVENT_BUS.receive().await;
-        match event {
-            MidiMessage::ProgramChange(_channel, prog) => {
-                handle_program_change(prog.into());
-            }
-            MidiMessage::ControlChange(channel, cc_number, cc_value) => {
-                handle_control_change(channel.into(), cc_number.into(), cc_value.into());
-            }
-            _ => {} // unhandled for now
-        }
-    }
-}
-
-#[embassy_executor::task]
-async fn flush_stop_state_to_leds(i2c_bus: &'static I2c0Bus) {
+async fn update_leds(i2c_bus: &'static I2c0Bus) {
     let mcps = &mut get_mcps(i2c_bus);
 
     loop {
-        // TODO: Think of a more efficient way to indicate which divisions need to be flushed.
-        let stop_state_change = select4(
-            STOP_STATE_CHANGED[0].wait(),
-            STOP_STATE_CHANGED[1].wait(),
-            STOP_STATE_CHANGED[2].wait(),
-            STOP_STATE_CHANGED[3].wait(),
-        )
-        .await;
-        let idx: usize = {
-            if stop_state_change.is_first() {
-                0
-            } else if stop_state_change.is_second() {
-                1
-            } else if stop_state_change.is_third() {
-                2
-            } else {
-                3
-            }
-        };
+        // Only update when things have changed.
+        STOP_STATE_CHANGED.wait().await;
 
-        let div_stop_state = STOP_STATE[idx].load(Ordering::SeqCst);
-        let left_half = reverse_byte(&(!(div_stop_state >> 8) as u8));
-        let right_half = reverse_byte(&(!(div_stop_state & 0xFF) as u8));
-        mcps[idx][0]
-            .write_gpio(Port::GPIOA, (left_half & 0x7F) << 1)
-            .unwrap();
-        mcps[idx][0]
-            .write_gpio(Port::GPIOB, left_half & 0x80)
-            .unwrap();
-        mcps[idx][1]
-            .write_gpio(Port::GPIOA, right_half & 0x7F)
-            .unwrap();
+        // This is our approach to batching lots of little changes: Wait for a brief interval before reading the stop
+        // states.
+        Timer::after_millis(1).await;
 
-        // This throttles updates a bit, so there might be mild lag when recalling presets (>= 4x this delay).
-        Timer::after_micros(200).await;
+        for div in 0..4 {
+            let div_stop_state = STOP_STATE[div].load(Ordering::SeqCst);
+            let left_half = reverse_byte(&(!(div_stop_state >> 8) as u8));
+            let right_half = reverse_byte(&(!(div_stop_state & 0xFF) as u8));
+            mcps[div][0]
+                .write_gpio(Port::GPIOA, (left_half & 0x7F) << 1)
+                .unwrap();
+            mcps[div][0]
+                .write_gpio(Port::GPIOB, left_half & 0x80)
+                .unwrap();
+            mcps[div][1]
+                .write_gpio(Port::GPIOA, right_half & 0x7F)
+                .unwrap();
+        }
     }
 }
 
@@ -197,19 +171,12 @@ async fn scan_stop_tab_buttons(i2c_bus: &'static I2c0Bus) {
             raw_reading |= ((mcp_pair[1].read_gpio(Port::GPIOB).unwrap() & 0x7F) as u16) << 1;
             stop_tab_button_debouncers[div].update(raw_reading);
 
-            // This iterator is almost always going to be size 0 or 1, so acquiring the mutex in the loop is fine.
             for i in stop_tab_button_debouncers[div].falling_edges() {
-                let div_stop_state = STOP_STATE[div].load(Ordering::SeqCst);
-                let mut midi_val = 0u8;
-                if div_stop_state >> i & 1 == 0 {
-                    midi_val = 127; // currently off, toggling to on
-                }
-                enqueue_midi_out_and_in(MidiMessage::ControlChange(
-                    MidiChannel::from(div as u8),
-                    Control::from(117 - i),
-                    Value7::from(midi_val),
-                ))
-                .await;
+                let div_stop_state = STOP_STATE[div].fetch_xor(1 << i, Ordering::SeqCst) ^ (1 << i);
+                STOP_STATE_CHANGED.signal(true);
+                OUTBOUND_MIDI_EVENT_BUS
+                    .send(stop_tab_midi_cc_message(div, i, div_stop_state))
+                    .await;
             }
         }
         Timer::after_micros(200).await;
@@ -223,8 +190,6 @@ async fn scan_pistons(
     data_pin: Peri<'static, AnyPin>,
 ) {
     let mut piston_debouncer = Debouncer::<5>::new(0xFFFF);
-    // TODO: Re-implement the save button using MIDI SysEx, so that it can live on a separate MCU and doesn't require
-    // special handling.
     let mut awaiting_save = false;
 
     // Load piston states into 74HC165 shift register
@@ -259,21 +224,18 @@ async fn scan_pistons(
                 awaiting_save = true;
             } else if i == 1 {
                 // GC piston, implemented as a "fixed preset" with all 0s
-                enqueue_midi_out_and_in(GC_MESSAGE).await;
+                OUTBOUND_MIDI_EVENT_BUS.send(GC_MESSAGE).await;
+                recall_preset(0).await;
             } else {
                 if awaiting_save {
-                    // write preset
-                    for div in 0..4 {
-                        PRESETS[i as usize - 1][div]
-                            .store(STOP_STATE[div].load(Ordering::SeqCst), Ordering::SeqCst);
-                    }
+                    // Saving a preset is completely internal; it emits no MIDI events.
+                    save_preset(i - 1);
                 } else {
-                    // recall preset
-                    enqueue_midi_out_and_in(MidiMessage::ProgramChange(
-                        MidiChannel::from(4), // should this be 5? am i off by one?
-                        Program::from(i - 1),
-                    ))
-                    .await;
+                    // Recalling a preset, on the other hand, emits a program change.
+                    let program_change =
+                        MidiMessage::ProgramChange(MidiChannel::from(4), Program::from(i - 1));
+                    OUTBOUND_MIDI_EVENT_BUS.send(program_change).await;
+                    recall_preset(i - 1).await;
                 }
             }
         }
@@ -392,8 +354,7 @@ async fn main(spawner: Spawner) {
         .spawn(scan_pistons(p.PIN_2.into(), p.PIN_6.into(), p.PIN_3.into()))
         .unwrap();
     spawner.spawn(scan_stop_tab_buttons(i2c_bus)).unwrap();
-    spawner.spawn(flush_stop_state_to_leds(i2c_bus)).unwrap();
-    spawner.spawn(midi_event_listener()).unwrap();
+    spawner.spawn(update_leds(i2c_bus)).unwrap();
     spawner.spawn(usb_midi_driver(spawner, p.USB)).unwrap();
 
     // Heartbeat task comes last, and happens on the main loop, roughly indicating that all tasks spawned successfully.
