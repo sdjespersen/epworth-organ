fa2e68f39372c956e4fa1977e17546c1c79e3482
Scott Jespersen <sjespersen@myst.ai>
2026-02-11 15:49:01 -0800

    Add crescendo logic on decoder.
    I already had this pretty much implemented, so mostly copy-paste.
    
    There is still a little room for improvement in noise reduction
    on the encoder.

--- src/bin/primary-decoder.rs
+++ src/bin/primary-decoder.rs
@@ -26,6 +26,62 @@ const NIBBLE_REVERSE_LOOKUP: [u8; 16] = [
     0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe, 0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf,
 ];
 
+const CRESCENDO_STEPS: usize = 39;
+const CRESCENDO_PEDAL_ORDER: [[u8; 2]; 38] = [
+    [3, 10], // Swell to Pedal 8'
+    [3, 11], // Swell to Pedal 4'
+    [3, 12], // Great to Pedal 8'
+    [3, 13], // Choir to Pedal 8'
+    [2, 14], // Choir to Choir 4'
+    [1, 9],  // Swell to Great 8'
+    [1, 12], // Choir to Great 8'
+    [1, 13], // Choir to Great 4'
+    [1, 2],  // Dulciana 8'
+    [0, 0],  // Gedeckt Flute 8'
+    [1, 4],  // Flute Ouverte 4'
+    [1, 1],  // Bourdon 8'
+    [3, 1],  // Gedekct 16'
+    [0, 1],  // Salicional 8'
+    [2, 1],  // Gemshorn 8'
+    [0, 2],  // Voix Celeste 8'
+    [0, 4],  // Harmonic Flute 4'
+    [0, 5],  // Block Flute 2'
+    [2, 4],  // Gemshorn 4'
+    [3, 2],  // Gemshorn 16'
+    [3, 3],  // Principal 8'
+    [2, 3],  // Koppel Flute 4'
+    [2, 0],  // Koppel Flute 8'
+    [1, 0],  // Principal 8'
+    [1, 3],  // Octave 4'
+    [1, 5],  // Fifteenth 2'
+    [2, 6],  // Koppel Flute 2'
+    [0, 3],  // Geigen Principal 8'
+    [3, 0],  // Open Diapason 16'
+    [1, 6],  // Plein Jeu IV (Fourniture)
+    [3, 7],  // Flute 4'
+    [0, 8],  // Clarion 4'
+    [3, 8],  // Contra Trompette 16'
+    [1, 10], // Swell to Great 4'
+    [0, 13], // Swell to Swell 4'
+    [2, 12], // Choir to Choir 16'
+    [0, 7],  // Trompette 8'
+    [0, 6],  // Scharf III-IV
+];
+
+const CRESCENDO_INDUCED_STATES: [u64; CRESCENDO_STEPS] = {
+    let mut states = [0u64; CRESCENDO_STEPS];
+    // This is essentially a for loop, but we can't use `Range` in const expressions.
+    let mut i = 0;
+    while i < CRESCENDO_STEPS - 1 {
+        states[i + 1] = states[i];
+        let div = CRESCENDO_PEDAL_ORDER[i][0] as usize;
+        let stop_bit = 16 * div + CRESCENDO_PEDAL_ORDER[i][1] as usize;
+        states[i + 1] |= 1 << stop_bit;
+        i += 1;
+    }
+    states
+};
+
 // Queue for all events on the decoder. The only way events make it onto the decoder is over UART.
 static EVENTS: Channel<CriticalSectionRawMutex, Event, 8> = Channel::new();
 static STOP_STATE: Signal<CriticalSectionRawMutex, u64> = Signal::new();
@@ -133,7 +189,7 @@ async fn uart_reader(mut uart_rx: uart::BufferedUartRx) {
 #[embassy_executor::task]
 async fn main_event_handler() {
     let mut stop_state: u64 = 0u64;
-    // let mut crescendo_induced_stop_state: u64 = 0u64;
+    let mut crescendo_induced_stop_state: u64 = 0u64;
 
     STOP_STATE.signal(stop_state);
 
@@ -147,30 +203,30 @@ async fn main_event_handler() {
                 defmt::info!("Received NoteOn div {:?} value {:?}", div as u8, value);
             }
             Event::StopOff(div, idx) => {
-                defmt::info!("Received StopOff div {:?} idx {:?}", div as u8, idx);
                 stop_state &= !(1 << 16 * div as u8 + idx);
-                STOP_STATE.signal(stop_state);
+                STOP_STATE.signal(stop_state | crescendo_induced_stop_state);
             }
             Event::StopOn(div, idx) => {
-                defmt::info!("Received StopOn div {:?} idx {:?}", div as u8, idx);
                 stop_state |= 1 << 16 * div as u8 + idx;
-                STOP_STATE.signal(stop_state);
+                STOP_STATE.signal(stop_state | crescendo_induced_stop_state);
             }
             Event::PresetRecalled(_, new_stop_state) => {
                 stop_state = new_stop_state;
-                STOP_STATE.signal(stop_state);
-                defmt::info!("Received PresetRecalled");
+                STOP_STATE.signal(stop_state | crescendo_induced_stop_state);
             }
             Event::GeneralCancel() => {
                 stop_state = 0;
-                STOP_STATE.signal(0);
-                defmt::info!("Received GeneralCancel");
+                STOP_STATE.signal(crescendo_induced_stop_state);
             }
             Event::Expression(div, value) => {
                 defmt::info!("Received Expression div {:?} value {:?}", div as u8, value);
             }
             Event::Crescendo(value) => {
-                defmt::info!("Received Crescendo value {:?}", value);
+                // By contract, value is guaranteed 0-127; map it down to 0-38.
+                // This is a little hacky, but i don't have stdlib for round/floor/ceil.
+                let raw_mapped = (value as u16 * 3 / 10) as usize;
+                crescendo_induced_stop_state = CRESCENDO_INDUCED_STATES[raw_mapped];
+                STOP_STATE.signal(stop_state | crescendo_induced_stop_state);
             }
         }
     }
