3edbb1a51d9fb3b7acc275ece686e311045f18fc
Scott Jespersen <scott.d.jespersen@gmail.com>
2026-03-07 16:29:08 -0800

    Progress checkin.
    Swell + pedal boards are currently fried. Waiting for replacement MCP23017s.
    
    Putting all logic on a single board for now, since we are abandoning all hope of MIDI for the moment.
    
    On the plus side, using 2.2k pull-ups on the i2c bus is working well, and getting rid of the pull-downs for address pins and hard-wiring them to +5V and GND is also a good move.

--- encoder-avr/src/main.rs
+++ encoder-avr/src/main.rs
@@ -6,8 +6,6 @@ mod mcp23017;
 
 use embedded_hal::i2c::I2c;
 use embedded_hal_bus::util::AtomicCell;
-use embedded_midi::{Channel, Control, MidiMessage, MidiOut};
-use midi_types::Value7;
 use panic_halt as _;
 
 use debouncer::Debouncer;
@@ -15,10 +13,31 @@ use debouncer::Debouncer;
 use embedded_hal_bus::i2c::AtomicDevice;
 use mcp23017::{MCP23017, Port};
 
+// Bit manipulation constants
+const B: [u32; 4] = [0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF];
+const S: [u32; 4] = [1, 2, 4, 8];
+const NIBBLE_REVERSE_LOOKUP: [u8; 16] = [
+    0x0, 0x8, 0x4, 0xc, 0x2, 0xa, 0x6, 0xe, 0x1, 0x9, 0x5, 0xd, 0x3, 0xb, 0x7, 0xf,
+];
+
+const SAVE_PISTON_IDX: u8 = 0;
+const GC_PISTON_IDX: u8 = 1;
+
 const STOP_TAB_BUTTON_POLL_ITVL_TICKS: u32 = 5;
 const PISTON_POLL_ITVL_TICKS: u32 = 5;
-const CRESCENDO_READ_ITVL_TICKS: u32 = 5;
 const LED_TOGGLE_ITVL_TICKS: u32 = 2_000;
+const FLUSH_STOP_STATE_ITVL_TICKS: u32 = 100;
+
+fn interleave_bits(a: u16, b: u16) -> u32 {
+    let mut x = a as u32;
+    let mut y = b as u32;
+
+    for i in (0..4).rev() {
+        x = (x | (x << S[i])) & B[i];
+        y = (y | (y << S[i])) & B[i];
+    }
+    x | (y << 1)
+}
 
 fn reverse_byte(a: &u8) -> u8 {
     let mut b = *a;
@@ -50,30 +69,10 @@ fn write_stop_state_to_leds<I2C: I2c>(stop_state: &u16, mcp_pair: &mut [MCP23017
         .unwrap();
 }
 
-fn stop_state_cc_message(div: usize, div_stop_state: u16, i: u8) -> MidiMessage {
-    let mut control_value = 0u8;
-    if (div_stop_state >> i) & 1 != 0 {
-        control_value = 127
-    };
-
-    // This is part of the MIDI contract that needs to be documented: Stop tabs use the undefined
-    // MIDI CCs 102-116 (inclusive), with channel equal to 1-indexed division, value 127 for ON, 0
-    // for OFF.
-    MidiMessage::ControlChange(
-        Channel::from(div as u8),
-        Control::from(117 - i),
-        Value7::from(control_value),
-    )
-}
-
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-
-    let serial = arduino_hal::default_serial!(dp, pins, 31250);
-    let (_rx, tx) = serial.split();
-    let mut midi_out = MidiOut::new(tx);
 
     let i2c = arduino_hal::i2c::I2c::new(
         dp.TWI,
@@ -84,14 +83,22 @@ fn main() -> ! {
     let i2c_cell = AtomicCell::new(i2c);
 
     let mut mcps = [
+        // [
+        //     MCP23017::new(AtomicDevice::new(&i2c_cell), 0x20),
+        //     MCP23017::new(AtomicDevice::new(&i2c_cell), 0x21),
+        // ],
         [
             MCP23017::new(AtomicDevice::new(&i2c_cell), 0x22),
             MCP23017::new(AtomicDevice::new(&i2c_cell), 0x23),
         ],
         [
-            MCP23017::new(AtomicDevice::new(&i2c_cell), 0x26),
-            MCP23017::new(AtomicDevice::new(&i2c_cell), 0x27),
+            MCP23017::new(AtomicDevice::new(&i2c_cell), 0x24),
+            MCP23017::new(AtomicDevice::new(&i2c_cell), 0x25),
         ],
+        // [
+        //     MCP23017::new(AtomicDevice::new(&i2c_cell), 0x26),
+        //     MCP23017::new(AtomicDevice::new(&i2c_cell), 0x27),
+        // ],
     ];
     for mcp_pair in mcps.iter_mut() {
         mcp_pair[0].init_hardware().unwrap();
@@ -110,18 +117,22 @@ fn main() -> ! {
 
     let mut onboard_led = pins.d13.into_output();
 
+    let piston_data_pin = pins.d8.into_pull_up_input();
+    let mut piston_clock_pin = pins.d9.into_output();
     let mut piston_load_pin = pins.d10.into_output();
-    let piston_data_pin = pins.d9.into_pull_up_input();
-    let mut piston_clock_pin = pins.d8.into_output();
 
-    // Set up ADC and the potentiometer wiper on pin A5
-    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
-    let crescendo_pin = pins.a3.into_analog_input(&mut adc);
+    // For controlling the shift registers
+    let mut oe = pins.d5.into_output();
+    let mut clk = pins.d4.into_output();
+    let mut latch = pins.d3.into_output();
+    let mut dta = pins.d2.into_output();
+
+    oe.set_high(); // Disable outputs initially
 
     let mut ticks = 0u32;
 
-    let mut smoothed_cresc_value = 0u16;
-    let mut last_cresc_control_value = 0u8;
+    oe.set_low();
+
     let mut awaiting_save_preset = false;
     let mut presets = [[0u16; 4]; 8];
 
@@ -132,6 +143,7 @@ fn main() -> ! {
         Debouncer::<5>::new(0xFFFF),
     ];
     let mut stop_state: [u16; 4] = [0u16; 4];
+    let mut pending_changes = true;
 
     let mut piston_debouncer = Debouncer::<5>::new(0xFFFF);
 
@@ -142,7 +154,7 @@ fn main() -> ! {
         }
 
         // Stop tabs
-        if ticks % STOP_TAB_BUTTON_POLL_ITVL_TICKS == 0{
+        if ticks % STOP_TAB_BUTTON_POLL_ITVL_TICKS == 0 {
             // Stop tab buttons for each division are logically separate, so this looks like 4 scans.
             for (div, mcp_pair) in mcps.iter_mut().enumerate() {
                 stop_tab_button_debouncers[div].update(read_stop_tabs(mcp_pair));
@@ -151,9 +163,7 @@ fn main() -> ! {
 
                     write_stop_state_to_leds(&stop_state[div], mcp_pair);
 
-                    midi_out
-                        .write(&stop_state_cc_message(div, stop_state[div], i))
-                        .unwrap();
+                    pending_changes = true;
                 });
             }
         }
@@ -182,67 +192,72 @@ fn main() -> ! {
             piston_debouncer.update(raw_piston_reading);
 
             piston_debouncer.for_each_falling_edge(|i| {
-                if i == 0 {
+                if i == SAVE_PISTON_IDX {
                     // SAVE piston
                     awaiting_save_preset = true;
-                } else if i == 1 {
+                } else if i == GC_PISTON_IDX {
                     // GC piston
                     for (div, mcp_pair) in mcps.iter_mut().enumerate() {
                         stop_state[div] = 0;
                         write_stop_state_to_leds(&stop_state[div], mcp_pair);
-                        for j in 0..16 {
-                            midi_out
-                                .write(&stop_state_cc_message(div, 0u16, j))
-                                .unwrap();
-                        }
                     }
+                    pending_changes = true;
                 } else {
                     if awaiting_save_preset {
                         // write preset
                         for div in 0..4 {
                             presets[i as usize][div] = stop_state[div];
                         }
+                        // Force the user to release and re-engage the save button to save another preset.
+                        awaiting_save_preset = false;
                     } else {
                         // recall preset
                         for (div, mcp_pair) in mcps.iter_mut().enumerate() {
                             stop_state[div] = presets[i as usize][div];
                             write_stop_state_to_leds(&stop_state[div], mcp_pair);
-                            for j in 0..16 {
-                                midi_out
-                                    .write(&stop_state_cc_message(div, stop_state[div], j))
-                                    .unwrap();
-                            }
                         }
+                        pending_changes = true;
                     }
                 }
             });
 
             piston_debouncer.for_each_rising_edge(|i| {
-                if i == 0 {
+                if i == SAVE_PISTON_IDX {
                     awaiting_save_preset = false;
                 }
             });
         }
 
-        // Crescendo pedal
-        if ticks % CRESCENDO_READ_ITVL_TICKS == 0 {
-            let crescendo_reading = crescendo_pin.analog_read(&mut adc) as u16;
+        if pending_changes && (ticks % FLUSH_STOP_STATE_ITVL_TICKS == 0) {
+            pending_changes = false;
 
-            smoothed_cresc_value = (smoothed_cresc_value * 3 + crescendo_reading) / 4;
+            // Interleave States
+            let left_half = interleave_bits(stop_state[1], stop_state[2]);
+            let right_half = interleave_bits(stop_state[3], stop_state[0]);
 
-            // Should be in [0, 127] since reading is [0, 1023]
-            let control_value = ((smoothed_cresc_value / 8) as u8) & 0x7f;
+            // Shift Out Right Half then Left Half
+            for val in [right_half, left_half] {
+                for i in 0..4 {
+                    let mut to_write = ((val >> (8 * i)) & 0xFF) as u8;
+                    // Reverse lower nibble
+                    to_write =
+                        (to_write & 0xF0) | NIBBLE_REVERSE_LOOKUP[(to_write & 0x0F) as usize];
 
-            if control_value != last_cresc_control_value {
-                let cc_msg = MidiMessage::ControlChange(
-                    Channel::from(4),  // channel 5 ("whole organ")
-                    Control::from(11), // expression
-                    Value7::from(control_value),
-                );
-                midi_out.write(&cc_msg).unwrap();
-
-                last_cresc_control_value = control_value;
+                    // Manual ShiftOut
+                    for bit in 0..8 {
+                        if (to_write >> bit) & 1 == 1 {
+                            dta.set_high();
+                        } else {
+                            dta.set_low();
+                        }
+                        clk.set_high();
+                        clk.set_low();
+                    }
+                }
             }
+
+            latch.set_high();
+            latch.set_low();
         }
 
         // I'd love to use async rust, but i'm not really sure how to do that on AVR yet. In the meantime, we'll just
